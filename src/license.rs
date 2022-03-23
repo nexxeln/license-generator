1de7209e51dc3bb2a123cbac3eac23b56e4fdf4a
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-23 23:52:56 +0530

    add function to get license content

--- src/license.rs
+++ src/license.rs
@@ -11,6 +11,28 @@ pub struct License {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct LicenseContent {
+    pub key: String,
+    pub name: String,
+    pub description: String,
+    pub permissions: Vec<String>,
+    pub conditions: Vec<String>,
+    pub limitations: Vec<String>,
+    pub body: String,
+}
+
+impl LicenseContent {
+    pub fn about(url: &String) -> LicenseContent {
+        let license_content: LicenseContent = match ureq::get(&url).call() {
+            Ok(res) => res.into_json().unwrap(),
+            Err(error) => panic!("Unable to fetch license content: {}", error)
+        };
+
+        license_content
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Licenses {
     pub license: Vec<License>
 }
@@ -28,4 +50,17 @@ impl Licenses {
     pub fn get_license_names(&self) -> Vec<String> {
         self.license.iter().map(|l| String::from(&l.name)).collect()
     }
+
+    pub fn get_license_from_name(&self, name: &String) -> LicenseContent {
+        let lisc = &self.license;
+
+        let result = lisc
+            .into_iter()
+            .filter(|l| l.name == name.clone())
+            .map(|l| l.url.clone())
+            .collect();
+
+        LicenseContent::about(&result)
+    }
 }
+
