c8f06f8ac482c3b23eb235f610ef063ef4968b3e
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-24 15:50:19 +0530

    clean up functions, fix bugs, comment code

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,11 +1,47 @@
-use std::{process::Command, io, fs};
+use std::{process::Command, fs, io};
 use dialoguer::{console::Style, theme::ColorfulTheme, Input, Select};
 use license::LicenseContent;
 use crate::license;
 
+// main logic to fill the license with name and year
+pub fn fill_content(license: &LicenseContent) {
+    let name = get_name();
+    let year = get_current_year();
+
+    // replacing content
+    let body = license
+        .body
+        .replace("[year]", &year)
+        .replace("[yyyy]", &year)
+        .replace("[fullname]", &name)
+        .replace("[name of copyright owner]", &name)
+        .replace("<year>", &year)
+        .replace("<name of author>", &name);
+
+    // write file according to replaced content
+    match write_file("LICENSE", &body) {
+        Ok(_) => println!(
+            "{}",
+            Style::new()
+                .for_stderr()
+                .green()
+                .apply_to("✔ License created successfully\n  Please take a look at it and make changes if necessary")
+        ),
+        Err(error) => println!(
+            "{} {}",
+            Style::new()
+                .for_stderr()
+                .red()
+                .apply_to("✘ An error occured"),
+            error
+        ),
+    };
+}
+
+// select license
 pub fn select(selections: &Vec<String>) -> String {
     let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select a license")
+        .with_prompt("Choose a license")
         .default(0)
         .items(&selections[..])
         .interact()
@@ -14,67 +50,38 @@ pub fn select(selections: &Vec<String>) -> String {
     selections[selection].clone()
 }
 
-pub fn fill_license(license: &LicenseContent) {
-    let name = get_name();
-    let year = get_current_year();
-
-    let content = license
-        .body
-        .replace("[year]", &year)
-        .replace("<year>", &year)
-        .replace("[fullname]", &name)
-        .replace("<name of author>", &name);
-
-    match write_file("LICENSE", &content) {
-        Ok(_) => println!(
-            "{}",
-            Style::new()
-                .for_stderr()
-                .green()
-                .apply_to("✓ License created successfully")
-        ),
-        Err(err) => println!(
-            "{} {}",
-            Style::new()
-                .for_stderr()
-                .red()
-                .apply_to("✘ an error occured"),
-            err
-        ),
-    };
-}
-
-fn get_current_year() -> String {
-    let year: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter the year")
-        .default("2022".to_string())
-        .interact_text()
-        .unwrap();
-
-    year
-}
-
-// try to get username from git config
-fn get_username() -> Option<String> {
+// try to get username from global git config
+fn get_git_username() -> Option<String> {
     let cmd = Command::new("git")
         .arg("config")
         .arg("--global")
         .arg("--get")
         .arg("user.name")
         .output()
-        .expect("failed to get username");
+        .expect("fail");
 
-    let result: Option<String> = match cmd.status.success() {
+    let res: Option<String> = match cmd.status.success() {
         true => Option::from(String::from_utf8_lossy(&cmd.stdout).to_string()),
-        false => Option::from(String::from(None)),
+        false => Option::from(None),
     };
 
-    result
+    res
 }
 
+// get name from user
 fn get_name() -> String {
-    let name: String = match get_username() {
-        Some(name) => {
+    let name: String = match get_git_username() {
+        Some(mut name) => {
+
+            // removing trailing newline (cross platform way)
+            if name.ends_with("\n") {
+                name.pop();
+
+                if name.ends_with("\r") {
+                    name.pop();
+                }
+            }
+
             let name: String = Input::with_theme(&ColorfulTheme::default())
                 .with_prompt("Enter your name")
                 .default(name)
@@ -85,7 +92,7 @@ fn get_name() -> String {
         }
         None => {
             let input: String = Input::with_theme(&ColorfulTheme::default())
-                .with_prompt("Your name")
+                .with_prompt("Name")
                 .interact_text()
                 .unwrap();
 
@@ -96,11 +103,23 @@ fn get_name() -> String {
     name
 }
 
+// get year from user
+fn get_current_year() -> String {
+    let year: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Enter year")
+        .default("2022".to_string())
+        .interact_text()
+        .unwrap();
+
+    year
+}
+
+// write license file
 fn write_file(path: &str, content: &str) -> Result<(), io::Error> {
     let result = match !fs::metadata(path).is_ok() {
         false => {
             let path: String = Input::with_theme(&ColorfulTheme::default())
-                .with_prompt("LICENSE already exists, enter a new name or the content will be overriden!")
+                .with_prompt("LICENSE already exists, enter a new name else the content will be overridden!")
                 .default(path.to_string())
                 .interact_text()
                 .unwrap();
